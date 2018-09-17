205b9080e2f4b4121f83b766cb31730c32697c42
Tim McCowan <tim.mccowan@AaAAAaaaaAAaAaahhhHhHh.local>
2018-09-17 15:24:00 -0400

    slope Calc update

--- SlopeCalculator.py
+++ SlopeCalculator.py
@@ -1,1 +1,19 @@
 print("Slope Calculator:")
+
+#Input
+x1 = input("Input x1: ")
+x1 = int(x1)
+
+y1 = input("Input x2: ")
+y1 = int(y1)
+
+x2 = input("Input x2: ")
+x2 = int(x2)
+
+y2 = input("Input y2: ")
+y2 = int(y2)
+
+#Process
+rise = x2 - x1
+
+#output
