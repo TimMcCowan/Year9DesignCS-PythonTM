4f0312311b1a57562ad7c8cfaf29618f051eaf8c
Tim McCowan <tim.mccowan@AaAAAaaaaAAaAaahhhHhHh.local>
2018-10-12 09:23:59 -0400

    made background colors nicer, worked on the look of program.

---
+++ IfDemo.py
@@ -0,0 +1,7 @@
+A = 10
+
+B = 10
+
+
+if A ==10 and B ==10:
+	print("Success!")
--- SwimCalc.py
+++ SwimCalc.py
@@ -1,9 +1,4 @@
-#swim Calc
-#measuring distance swam in meters or yards 
-#Gui with multiples of 20(y) or 25(m) for recording sets with conversions
-#Save inputs for weekly reports which contain goals and maybe graphs
-#Also possibility of calories
-#swim.com smart swim tracking
+
 
 import tkinter as tk
 import math
@@ -26,6 +21,15 @@ def submit():
 	ttl =d1 +d2 + d3+d4+d5+d6+d7
 
 	u = str(v.get())
+	if u == "Meters" and ttl >1000:
+		u = "Kilometers"
+		ttl = ttl/1000	
+
+	if u == "Yards" and ttl>1760:
+		u = "Miles"
+		ttl = ttl/1760
+
+	ttl = round(ttl,2)
 	print("You swam " +str(ttl)+" " +str(u)+" this week")
 
 
@@ -33,6 +37,8 @@ def submit():
 
 root = tk.Tk()
 
+
+root.configure(bg='blue')
 #radio button options
 MODES = [
 ("Meters", "Meters"),
@@ -43,7 +49,7 @@ v = tk.StringVar()
 v.set("A")
 
 #label 1
-t1 = tk.Label(root,text = "Enter distance swam for workout 1")
+t1 = tk.Label(root,text = "Enter distance swam for workout 1",bg = 'blue')
 t1.pack()
 
 #text box 1 
@@ -51,7 +57,7 @@ e1 = tk.Entry(root)
 e1.pack()
 
 # label 2
-t2 = tk.Label(root,text = "Enter distance swam for workout 2")
+t2 = tk.Label(root,text = "Enter distance swam for workout 2",bg = 'blue')
 t2.pack()
 
 #text box 2 
@@ -59,7 +65,7 @@ e2 = tk.Entry(root)
 e2.pack()
 
 # label 3
-t3 = tk.Label(root,text = "Enter distance swam for workout 3")
+t3 = tk.Label(root,text = "Enter distance swam for workout 3",bg = 'blue')
 t3.pack()
 
 #text box 3
@@ -67,7 +73,7 @@ e3 = tk.Entry(root)
 e3.pack()
 
 # label 4
-t4 = tk.Label(root,text = "Enter distance swam for workout 4")
+t4 = tk.Label(root,text = "Enter distance swam for workout 4",bg = 'blue')
 t4.pack()
 
 #text box 4
@@ -75,15 +81,15 @@ e4 = tk.Entry(root)
 e4.pack()
 
 # label 5
-t5 = tk.Label(root,text = "Enter distance swam for workout 5")
+t5 = tk.Label(root,text = "Enter distance swam for workout 5",bg = 'blue')
 t5.pack()
 
-#text box 3
+#text box 5
 e5 = tk.Entry(root)
 e5.pack()
 
 # label 6
-t6 = tk.Label(root,text = "Enter distance swam for workout 6")
+t6 = tk.Label(root,text = "Enter distance swam for workout 6",bg = 'blue')
 t6.pack()
 
 #text box 6
@@ -91,10 +97,10 @@ e6 = tk.Entry(root)
 e6.pack()
 
 # label 7
-t7 = tk.Label(root,text = "Enter distance swam for workout 7")
+t7 = tk.Label(root,text = "Enter distance swam for workout 7",bg = 'blue')
 t7.pack()
 
-#text box 3
+#text box 7
 e7 = tk.Entry(root)
 e7.pack()
 
@@ -102,11 +108,11 @@ e7.pack()
 
 #choose unit
 for r in range(0,len(MODES),1):
-	rbtn = tk.Radiobutton(root,text = MODES[r][0],variable = v, value=MODES[r][1])
+	rbtn = tk.Radiobutton(root,text = MODES[r][0],variable = v, value=MODES[r][1],bg = 'gray')
 	rbtn.pack()
 
 #submit button
-btn = tk.Button(root,text = "Submit", command = submit)
+btn = tk.Button(root,text = "Submit", command = submit,bg = 'gray',borderwidth = 0.001,)
 btn.pack()
 
 
