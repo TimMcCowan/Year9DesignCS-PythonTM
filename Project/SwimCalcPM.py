f3633adb8c5b9e47c4deffb8ff3f288136c23a15
Tim McCowan <tim.mccowan@AaAAAaaaaAAaAaahhhHhHh.local>
2018-12-11 18:16:15 -0500

    reworked gui and made program functional

--- Project/SwimCalcPM.py
+++ Project/SwimCalcPM.py
@@ -2,6 +2,59 @@ import tkinter as tk
 import math
 
 
+#user selected
+def user(*args):
+	print("user selected:"+var.get())
+
+
+#average
+def submit(*args):
+	print("Submit")
+	#Step 1: 
+	#Take everything from the text box THE USER MUST HIT ENTER AFTER EACH ENTRY
+	val = distT.get("1.0",tk.END)
+
+	#Step 2: Remove all of the spaces form val
+	print(val)
+	list1 = val.split("\n") #Turns it into a list, separating on the \n a new line
+
+	#print(list1)
+	
+	a = 0
+	for i in range(len(list1)-1):
+		list1[i] = int(list1[i])
+		a += list1[i]
+	
+	b = a/len(list1)
+	print("user selected: " +var.get())
+	print("your average distance per workout was: {0} " .format(b) + v.get() )
+	outpt.delete("1.0",tk.END)
+	outpt.insert(tk.END, "average = "+str(round(b,2)))
+	
+
+	outpt1.delete("1.0", tk.END)
+	outpt1.insert(tk.END,"total = "+str(a))
+	#UPDATE OUR DATA LISTS
+	#I have multiple entries per submit
+	#Step 3: 
+	#Check the last elemet of list to ensure it is NOT a '' - does this as long as it is true
+	while list1[len(list1) - 1] == '':
+		list1.pop(len(list1) - 1) #removes the last element
+
+	
+	#Step 4: Update everything
+
+	for i in range(0, len(list1),1):
+		userList.append(var.get())
+		unitsList.append(list1[i])
+		#distanceList.append()
+
+
+#Variables that store information
+#User
+userList = []
+unitsList = []
+distanceList = [] 
 
 root = tk.Tk()
 
@@ -13,23 +66,17 @@ MODES = [
 
 DDM = ["Tim","John","Jack"]
 
-def user(*args):
-	print("user selected:"+var.get())
-
-def submit(*args):
-	print(distT.get("1.0",tk.END))	
-
 var = tk.StringVar(root)
 var.set(DDM[0])
 var.trace("w",user)
 
 
 # workout text
-wrkt = tk.Label(root, text= "Type in number of workouts")
-wrkt.grid(column = 0, row  = 0)
+#wrkt = tk.Label(root, text= "Type in number of workouts")
+#wrkt.grid(column = 0, row  = 0)
 
-wrktT = tk.Text(root, bg = 'grey')
-wrktT.grid(column = 0, row = 1)
+#wrktT = tk.Text(root, bg = 'grey', height = 1, width = 3)
+#wrktT.grid(column = 0, row = 1)
 
 
 #Dropdown menu
@@ -42,16 +89,16 @@ userT.grid(column = 1, row = 1, columnspan = 2)
 
 #Distance text
 dist = tk.Label(root, text ="distance")
-dist.grid(column = 0, row = 2,)
+dist.grid(column = 0, row = 2)
 
-distT = tk.Text(root,bg = 'grey', height = 10)
+distT = tk.Text(root,bg = 'grey', height = 10, width = 5)
 distT.grid(column = 0, row = 3 )
 
 
 
 
 v = tk.StringVar()
-
+v.set(MODES[0][0])
 
 
 #RADIO BUTTONS
@@ -64,7 +111,12 @@ for r in range(0,len(MODES),1):
 smbtn = tk.Button(root, text = "Submit!", command = submit)
 smbtn.grid(row = 4, column = 1,rowspan = 2)
 
+outpt = tk.Text(root, height = 2, width = 10,background = "light grey")
+outpt.grid(column = 0, row = 6)
 
+
+outpt1 = tk.Text(root, height = 2, width = 10,background = "light grey")
+outpt1.grid(column = 1, row = 6)
 
 
 
