6f8e645d78a9c7150d19128e79563287eb03022c
Tim McCowan <tim.mccowan@AaAAAaaaaAAaAaahhhHhHh.local>
2018-12-14 11:06:03 -0500

    added accessability options, average now works

--- Project/SwimCalcPM.py
+++ Project/SwimCalcPM.py
@@ -21,19 +21,30 @@ def submit(*args):
 	#print(list1)
 	
 	a = 0
+	b =0
 	for i in range(len(list1)-1):
 		list1[i] = int(list1[i])
 		a += list1[i]
 	
-	b = a/len(list1)
+	
+	u = v.get()
+
+	if u == "Meters" and a >=1000:
+		u = "Kilometers"
+		a = a/1000	
+
+	if u == "Yards" and a>=1760:
+		u = "Miles"
+		a = a/1760
+	b = a/(len(list1)-1)
 	print("user selected: " +var.get())
-	print("your average distance per workout was: {0} " .format(b) + v.get() )
+	print("your average distance per workout was: {0} " .format(b) +" "+ u )
 	outpt.delete("1.0",tk.END)
-	outpt.insert(tk.END, "average = "+str(round(b,2)))
+	outpt.insert(tk.END, "average = \n {0} " .format(b)+" "+u)
 	
 
 	outpt1.delete("1.0", tk.END)
-	outpt1.insert(tk.END,"total = "+str(a))
+	outpt1.insert(tk.END,"total = \n"+str(a) +" "+ u)
 	#UPDATE OUR DATA LISTS
 	#I have multiple entries per submit
 	#Step 3: 
@@ -47,7 +58,38 @@ def submit(*args):
 	for i in range(0, len(list1),1):
 		userList.append(var.get())
 		unitsList.append(list1[i])
-		#distanceList.append()
+#font
+f = 1
+
+def fontup(*args):
+	global f
+	f = f+1
+	a = f
+	fontsize3.configure(text=f)
+
+	user.configure(font=f)
+	dist.configure(font=f)
+	rbtn.configure(font=f)
+	smbtn.configure(font=f)
+	outpt.configure(font=f)
+	fontsize1.configure(font=f)
+	fontsize2.configure(font=f)
+	fontsize3.configure(font=f)
+
+def fontdown(*args):
+	global f
+	f = f-1
+	fontsize3.configure(text=f)
+
+	user.configure(font=f)
+	dist.configure(font=f)
+	rbtn.configure(font=f)
+	smbtn.configure(font=f)
+	outpt.configure(font=f)
+	fontsize1.configure(font=f)
+	fontsize2.configure(font=f)
+	fontsize3.configure(font=f)
+
 
 
 #Variables that store information
@@ -95,8 +137,6 @@ distT = tk.Text(root,bg = 'grey', height = 10, width = 5)
 distT.grid(column = 0, row = 3 )
 
 
-
-
 v = tk.StringVar()
 v.set(MODES[0][0])
 
@@ -111,12 +151,23 @@ for r in range(0,len(MODES),1):
 smbtn = tk.Button(root, text = "Submit!", command = submit)
 smbtn.grid(row = 4, column = 1,rowspan = 2)
 
-outpt = tk.Text(root, height = 2, width = 10,background = "light grey")
+outpt = tk.Text(root, height = 2, width = 15,background = "light grey",font = f)
 outpt.grid(column = 0, row = 6)
 
 
-outpt1 = tk.Text(root, height = 2, width = 10,background = "light grey")
+outpt1 = tk.Text(root, height = 2, width = 15,background = "light grey")
 outpt1.grid(column = 1, row = 6)
+
+fontsizeL = tk.Label(root, text = "Accesibility: Font size")
+fontsize1 = tk.Button(root, command=lambda: fontdown(),background = "light grey", text = "-") 
+fontsize3 = tk.Label(root, text=str(f))
+fontsize2 = tk.Button(root, command =lambda: fontup(),background = "light grey", text = "+")
+
+
+fontsizeL.grid(column = 1, row = 7)
+fontsize1.grid(column = 0, row  =8)
+fontsize3.grid(column = 1,row = 8)
+fontsize2.grid(column = 2, row =8) 
 
 
 
